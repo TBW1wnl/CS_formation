d169962e75e51c610c81a72b2321c3fe7cfdfee3
Thomas WEBER <alpha.tbw@gmail.com>
2025-12-10 14:44:45 +0100

    feat: Display board and first move

---
+++ TD1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace TD_Morpion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Game board = new();
+        char joueurActuel = 'X';
+
+        while (true)
+        {
+            board.Display();
+            Console.WriteLine($"\nJoueur {joueurActuel}, choisissez une position (1-9) : ");
+
+            if (int.TryParse(Console.ReadLine(), out int position))
+            {
+                if (board.PlayMove(position, joueurActuel))
+                {
+                    joueurActuel = (joueurActuel == 'X') ? 'O' : 'X';
+                }
+                else
+                {
+                    Console.WriteLine("Appuyez sur une touche pour réessayer...");
+                    Console.ReadKey();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrée invalide ! Appuyez sur une touche pour réessayer...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
